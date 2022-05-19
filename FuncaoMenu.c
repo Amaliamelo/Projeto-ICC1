a5a350c9119426861549881bc20e9173772acd39
Amaliamelo <amalia.melo@aluno.ifsp.edu.br>
2022-05-19 09:48:32 -0300

    criando a função iniciar

--- FuncaoMenu.c
+++ FuncaoMenu.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 int menu();
 
+
+//função principal
 int main (){
     int x = menu();
-    printf("VocÃª escolheu %d\n", x);
-
+    printf("Você escolheu %d\n", x);
+    if (x==1){
+    	inciar();
+	}
+	else{
+		if(x==2){
+			continuar();
+		}
+		else{
+			sair();
+		}
+	}
 
     return 0;
 }
  
+ //Menu 1 
 int menu(){
     int EscolhaJogador;
     do{
@@ -23,4 +36,40 @@ int menu(){
             
     }while(EscolhaJogador != 3);
     return 0;
+}
+
+//Função Iniciar
+int iniciar(){
+	
+	char nome;
+	char nome_confirmado;
+	int confirmacao;
+	FILE * arq_usuario;
+	
+	printf("Seja Bem - Vindo(a)!!! Digite seu nome de usuário:");
+    scanf(" %s", &nome);
+    
+    printf("\n Seu nome de usuario é: %s", nome);
+    printf("\n Podemos confirmar?! \n 1 - sim  2 - nao");
+   	while(confirmacao != 1 || confirmacao != 2){
+	 	printf("Valor incorreto, digite novamente\n");
+	 	scanf("%d", &confirmacao);
+	}
+    
+    
+    if(confirmacao==1){
+    	if ( arq_usuario=fopen("usuario","r") )
+			{
+			while ( !feof(ArqIn) )
+			{
+			fscanf(ArqIn,"%c",&caracter);
+			printf("%c",caracter);
+			}
+			fclose(ArqIn);
+			}
+			else printf ("Erro na leitura do arquivo de entrada!")
+	}
+	else{
+		
+	}
 }
